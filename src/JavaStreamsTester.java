226adcbaa3f2146324f1cfd93142a9dd24cd9556
gschwendtnerj <gschwendtnerj190088@sus.htl-grieskirchen.at>
2021-10-17 19:56:29 +0200

    finished 3

--- src/JavaStreamsTester.java
+++ src/JavaStreamsTester.java
@@ -14,12 +14,18 @@ public class JavaStreamsTester {
     }
     private static List<String> deleteEmptyStrings(List<String> strings)
     {
-        return strings.stream().filter(value -> value.length()==0).collect(Collectors.toList());
+        return strings.stream().filter(value -> value.length()!=0).collect(Collectors.toList());
     }
 
     private static String getMergedString(List<String> strings, String seperator)
     {
-        return strings.stream().skip(0).map(s -> seperator+s).toString();
+        return strings.stream().filter(value -> value.length()!=0).map(s -> {
+            if(strings.get(0).equals(s))
+            {
+                return s;
+            }
+            return seperator+s;
+        }).collect(Collectors.joining());
     }
 
     private static List<Integer> getSquares(List<Integer> numbers)
@@ -34,7 +40,7 @@ public class JavaStreamsTester {
 
     private static int getMin(List<Integer> numbers)
     {
-        return numbers.stream().mapToInt(value -> value).sum();
+        return numbers.stream().min((o1, o2) -> o1 - o2).get();
     }
 
     private static int getSum(List<Integer> numbers)
@@ -60,8 +66,25 @@ public class JavaStreamsTester {
         System.out.println("getCountEmtpyString: " + (getCountEmptyString(stringList)==2));
         System.out.println("getCountLength3: " + (getCountLength3(stringList)==2));
         List<String> tempList = deleteEmptyStrings(stringList);
+        System.out.println("deleteEmptyStrings: " + (tempList.size()==3));
+        System.out.println("getMergedString: " + (getMergedString(stringList,":").equals("Hallo:123:Hal")));
 
+        List<Integer> intList = new ArrayList<>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        List<Integer> tempList2 = getSquares(intList);
+        boolean getSquares = true;
 
+        if(tempList2.get(0)!=1 || tempList2.get(1)!=4 || tempList2.get(2)!=9)
+        {
+            getSquares = false;
+        }
+        System.out.println("getSquares: " + getSquares);
+        System.out.println("getMax: " + (getMax(intList)==3));
+        System.out.println("getMin: " + (getMin(intList)==1));
+        System.out.println("getSum: " + (getSum(intList)==6));
+        System.out.println("getAverage: " + (getAverage(intList)==2));
 
     }
 }
